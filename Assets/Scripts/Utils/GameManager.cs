b5cb30e58b24f9297794d4bdc059d0aac73a1a8b
Denis <denis.botanov1@gmail.com>
2024-02-23 18:15:25 +0600

    minor tweaks

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -1,11 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-   //score tracker
-   //timer
-   //switch to the 2d scene
-   //pause menu
+    //score tracker
+    //timer  
+
+    //count down
+
+    public TMP_Text scoreText; // Ссылка на текстовое поле для отображения счета
+    public TMP_Text timerText; // Ссылка на текстовое поле для отображения таймера
+    public Slider timerSlider; // Ссылка на слайдер для отображения градиента таймера
+
+    // Другие переменные и методы игрового менеджера
+
+    public  int score = 0; // Переменная для отслеживания счета
+    private float counter = 0; // Начальное значение таймера
+    public float maxTime = 60f; // Начальное значение таймера
+
+
+    void Start()
+    {
+        counter = maxTime;
+        UpdateScoreUI(); // Обновляем отображение счета при запуске игры
+        UpdateTimerUI(); // Обновляем отображение таймера при запуске игры
+    }
+    void FixedUpdate()
+    {
+        // Update the timer value every FixedUpdate
+        UpdateTimer();
+        UpdateTimerUI();
+    }
+    // Метод для обновления отображения счета
+    void UpdateScoreUI()
+    {
+        scoreText.text = score.ToString(); // Обновляем текст счета
+    }
+
+    void UpdateTimer()
+    {
+        // Calculate the time elapsed between frames and subtract it from the timer
+        float timeElapsed = Time.fixedDeltaTime;
+        counter -= timeElapsed;
+
+        // If the timer goes below 0, clamp it to 0
+        if (counter < 0f)
+        {
+            counter = 0f;
+            // Optionally, you can handle game over logic here
+            Debug.Log("Time's up!");
+        }
+    }
+
+    void UpdateTimerUI()
+    {
+        int minutes = Mathf.FloorToInt(counter / 60);
+        int seconds = Mathf.FloorToInt(counter % 60);
+        string timerString = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timerText.text = timerString;
+
+        float fillAmount = counter / maxTime;
+        timerSlider.value = fillAmount;
+    }
+   
+
+    // Метод для увеличения счета
+    public void IncreaseScore(int points)
+    {
+        score += points; // Увеличиваем счет на указанное количество очков
+        UpdateScoreUI(); // Обновляем отображение счета
+    }
 }
